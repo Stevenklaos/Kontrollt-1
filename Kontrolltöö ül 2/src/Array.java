1a5128d1a524dec79db52263f434cdf17e6215d6
Stevenklaos <55531464+Stevenklaos@users.noreply.github.com>
2020-10-13 19:08:38 +0300

    Add files via upload

---
+++ Kontrolltöö ül 2/src/Array.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Array {
+    public class Array {
+        public static void array(String[] args) {
+            ArrayList<String> nimi = new ArrayList<String>();
+            ArrayList<String> liha = new ArrayList<String>();
+            ArrayList<String> sai = new ArrayList<String>();
+
+            nimi.add("Chicken burger");
+            nimi.add("Old Fashioned Cheeseburger");
+            nimi.add("New York Burger");
+
+            liha.add("Kanaliha");
+            liha.add("Loomaliha");
+            liha.add("Sealiha");
+
+            sai.add("Valge sai");
+            sai.add("Teraleib");
+        }
+    }
+}
---
+++ Kontrolltöö ül 2/src/Burger.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Burger {
+    public String nimi;
+    public String liha;
+    public String sai;
+    public double hind;
+}
